dfe6af916bf4890a6cfd42543310f94a931c4d5d
1linktec <jhope@1linktec.net>
2025-06-01 13:13:36 -0300

    Update __init__.py

--- components/heltec_lora/__init__.py
+++ components/heltec_lora/__init__.py
@@ -1,1 +1,14 @@
 
+from esphome import automation
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+heltec_ns = cg.esphome_ns.namespace("heltec_lora")
+HeltecLoRa = heltec_ns.class_("HeltecLoRaComponent", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({cv.GenerateID(): cv.declare_id(HeltecLoRa)})
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
